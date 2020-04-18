3d85f01abc9f46700248745053b18d7ebe6edcf0
Techzio <punithadevi.c@gmail.com>
2020-04-18 10:15:45 -0700

    Counter with Line Item
    This code has a counter buttons for increment and decrement with line item displayed based on count.

--- src/App.js
+++ src/App.js
@@ -2,25 +2,67 @@ import React from "react";
 import "./styles.css";
 
 class App extends React.Component {
-  state = {
-    showp: false
-  };
-  toggleShowp = () => {
-    this.setState(prevState => {
-      return { showp: !prevState.showp };
-    });
+  constructor() {
+    super();
+    this.state = {
+      count: 0
+    };
+  }
+
+  incrementCount = () => {
+    const maxcount = 8;
+    if (this.state.count === maxcount) {
+      this.setState({
+        count: 0
+      });
+    } else {
+      this.setState(prevState => {
+        return {
+          count: prevState.count + 1
+        };
+      });
+    }
   };
 
+  decrementCount = () => {
+    if (this.state.count) {
+      this.setState(prevState => {
+        return {
+          count: prevState.count - 1
+        };
+      });
+    }
+  };
   render() {
     return (
-      <React.Fragment>
-        <a href="#" onClick={this.toggleShowp}>
-          Want to buy a new car?
-        </a>
-        {this.state.showp ? <p>Call +11 22 33 44 now!</p> : null}
-      </React.Fragment>
+      <form>
+        <h1>{this.state.count}</h1>
+        <button
+          type="button"
+          name="incrementCount"
+          onClick={this.incrementCount}
+        >
+          +
+        </button>
+        <button
+          type="button"
+          name="decrementCount"
+          onClick={this.decrementCount}
+        >
+          -
+        </button>
+        <Lineitem count={this.state.count} />
+      </form>
     );
   }
 }
+
+const Lineitem = ({ count }) => {
+  const item = [];
+  for (let i = 0; i < count; i++) {
+    item.push(<li key={i}>This is line item {i + 1}</li>);
+  }
+  return <ul>{item}</ul>;
+};
 
 export default App;
